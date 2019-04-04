e56d7e691c5f6c63ab832a2780e165f1a7af175d
Zaramont <zaramont90@gmail.com>
2019-04-05 00:23:05 +0300

    draw module in process

--- seacombat_draw.py
+++ seacombat_draw.py
@@ -12,7 +12,7 @@ black_rectangle = '#000000'
 
 
 def init_gui():
-    global root
+    global root, container1, canvas, w, h
     root = tkinter.Tk()
     root.title("SeaCombat")
 
@@ -20,7 +20,8 @@ def init_gui():
     mw = root.winfo_screenwidth()
     mh = root.winfo_screenheight()
 
-    root.geometry("{}x{}+{}+{}".format(w + 220, h + 20, (mw - w) // 2, (mh - h) // 2))
+    root.geometry(
+        "{}x{}+{}+{}".format(w + 220, h + 20, (mw - w) // 2, (mh - h) // 2))
     root.resizable(0, 0)
 
     container1 = tkinter.Frame(master=root)
@@ -30,8 +31,20 @@ def init_gui():
     canvas.pack(fill=tkinter.BOTH, expand=True)
 
 
-def draw_board():
-    pass
+def draw_board(whitecolor=None, blackcolor=None):
+    rect_side = 25
+    coodr_x = coord_y = 50
+    canvas.create_rectangle(coodr_x, coord_y, coodr_x + rect_side * 10,
+                            coord_y + rect_side * 10)
+    # canvas.create_rectangle(350,50,600,300)
+    for r in range(1, 11):
+        for c in range(1, 11):
+            canvas.create_rectangle(coodr_x + (r - 1) * rect_side,
+                                    coord_y + (c - 1) * rect_side,
+                                    (coodr_x + (r - 1) * rect_side) + 25,
+                                    (coord_y + (c - 1) * rect_side) + 25)
+
+    canvas.create_rectangle(50, 50, 75, 75)
 
 
 if __name__ == '__main__':
