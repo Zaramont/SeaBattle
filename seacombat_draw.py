800dadc32dcdfc52a849986b55e88988b91ddd73
Zaramont <zaramont90@gmail.com>
2019-04-04 23:54:47 +0300

    backup =(

--- seacombat_draw.py
+++ seacombat_draw.py
@@ -1,6 +1,45 @@
 import tkinter
 
-tkinter._test()
+# глобальные переменные - виджеты
+root = None
+canvas = None
+# другие глобальные переменные
+board_size = 400
+background_color = '#f8f4ff'
+
+white_rectangle = "#FFFFFF"
+black_rectangle = '#000000'
+
+
+def init_gui():
+    global root
+    root = tkinter.Tk()
+    root.title("SeaCombat")
+
+    w = h = board_size
+    mw = root.winfo_screenwidth()
+    mh = root.winfo_screenheight()
+
+    root.geometry("{}x{}+{}+{}".format(w + 220, h + 20, (mw - w) // 2, (mh - h) // 2))
+    root.resizable(0, 0)
+
+    container1 = tkinter.Frame(master=root)
+    container1.place(x=0, y=0, width=w + 220, height=h + 20)
+
+    canvas = tkinter.Canvas(master=container1, background=background_color)
+    canvas.pack(fill=tkinter.BOTH, expand=True)
+
+
+def draw_board():
+    pass
+
+
+if __name__ == '__main__':
+    init_gui()
+    draw_board()
+
+root.mainloop()
+
 
 def show_battlefield(field):
     for r in range(len(field)):
