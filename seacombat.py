c64e2c08609bd16e81b2994c9ab92e7dd4991019
Aleksandr Puchek <zaramont90@gmail.com>
2019-04-08 12:00:12 +0300

    first launch

--- seacombat.py
+++ seacombat.py
@@ -4,4 +4,6 @@ import seacombat_logic
 # seacombat_draw.show_battlefield(seacombat_logic.field)
 # seacombat_draw.show_array(seacombat_logic.field)
 # seacombat_draw.start(seacombat_logic.get_blank_field(), seacombat_logic.get_arranged_ships())
-seacombat_draw.start(seacombat_logic.get_arranged_ships(), seacombat_logic.get_arranged_ships())
+# seacombat_draw.start(seacombat_logic.get_arranged_ships(), seacombat_logic.get_arranged_ships())
+
+print(seacombat_logic.is_ships_placement_legal(seacombat_logic.get_arranged_ships()))
--- seacombat_logic.py
+++ seacombat_logic.py
@@ -13,6 +13,58 @@ def can_place_ship(row, column, direction, size, field):
     return True
 
 
+def is_ships_placement_legal(field):
+    one_deck = 0
+    two_deck = 0
+    three_deck = 0
+    four_deck = 0
+    ships = [1, 2, 3, 4]
+
+    for r in field:
+        i = 1
+        while i < 11:
+            if field[r][i] in ships:
+
+                ship_size = field[r][i]
+                for s in range(ship_size):
+                    if field[r - 1][i + s - 1] in ships:  # check  that there aren't ships on diagonals
+                        return False
+                    if field[r - 1][i + s + 1] in ships:
+                        return False
+                    if field[r + 1][i + s - 1] in ships:
+                        return False
+                    if field[r + 1][i + s + 1] in ships:
+                        return False
+
+                    if s + 1 == ship_size:
+                        if field[r][i + s + 1] != 9:
+                            return False
+                        if field[r - 1][i + s] != 9 or field[r + 1][i + s] != 9:
+                            return False
+                        i = i + ship_size
+
+                        if ship_size == 1:
+                            one_deck += 1
+                        elif ship_size == 2:
+                            two_deck += 1
+                        elif ship_size == 3:
+                            three_deck += 1
+                        elif ship_size == 4:
+                            four_deck += 1
+                        continue
+
+                    if field[r][i + s + 1] == ship_size:
+                        if field[r - 1][i + s] != 9 or field[r + 1][i + s] != 9:
+                            return False
+                    elif field[r][i + s + 1] == 9:
+                        if field[r - 1][i + s] != ship_size or field[r + 1][i + s] != ship_size:
+                            i += 1
+
+    if one_deck > 4 or two_deck > 3 or three_deck > 2 or four_deck > 1:
+        return False
+    return True
+
+
 def place_ship(row, column, direction, size, field):
     if direction == 1:
         for r in range(row - 1, row + 2):
@@ -28,6 +80,7 @@ def place_ship(row, column, direction, size, field):
                     field[r][c] = size
                 else:
                     field[r][c] = 9
+
 
 def get_blank_field():
     field = [
@@ -45,6 +98,7 @@ def get_blank_field():
         [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
     ]
     return field
+
 
 def get_arranged_ships():
     size = 4
